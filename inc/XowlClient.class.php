1a525a7dc8a71704526adca18c317a452eea0ce2
Juan Manuel Rodríguez <jmrodriguez@ximdex.net>
2015-06-15 14:13:20 +0200

    fix year of publication in license comment

--- inc/XowlClient.class.php
+++ inc/XowlClient.class.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- *  \details © 2014  Open Ximdex Evolution SL [http://www.ximdex.org]
+ *  \details © 2015  Open Ximdex Evolution SL [http://www.ximdex.org]
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published
