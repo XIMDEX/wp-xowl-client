7717d7f33dcc2a892662a14c30be623cd319ea34
DrZippie <antonio@antoniocortes.com>
2015-06-16 22:19:43 +0200

    Auto load CSS (without click) Decoupling access to editor Dynamic change of URIS

--- tinymce/xowl_client/editor_plugin.js
+++ tinymce/xowl_client/editor_plugin.js
@@ -1,90 +1,129 @@
 /**
  * plugin for tinymce and wordpress accessing xowl service
  */
-(function ($) {
+(function($) {
+    // clean contents before send to Xowl Service 
+    var XowlService = function() {
+        this.semantic = [];
+    }
+    XowlService.prototype.preClean = function() {
+        var self = this;
+        var $content = $('<div></div>').append($.parseHTML(this.getContent()));
+        $('a', $content).each(function() {
+            if (typeof $(this).attr("data-cke-suggestions") == 'string') {
+                $(this).removeAttr("data-cke-suggestions");
+                $(this).removeAttr("data-entity-position");
+                $(this).removeAttr("data-cke-annotation");
+                $(this).removeAttr("data-cke-type");
+                $(this).removeClass("xowl-suggestion");
+            } else {
+                return;
+            }
+        });
+        return $content.html();
+    };
+    XowlService.prototype.buildFromData = function(data) {
+        var self = this;
+        self.semantic = data.semantic;
+        return data.text;
+    };
+    XowlService.prototype.getContent = function() {
+        return tinymce.activeEditor.getContent();
+    }
+    XowlService.prototype.setContent = function(content) {
+        tinymce.activeEditor.setContent(content);
+    }
+    XowlService.prototype.postReceive = function(content) {
+        // returns string 
+        var $content = $('<div></div>').append($.parseHTML(content));
+        var count = 0;
+        $('a.xowl-suggestion', $content).each(function() {
+            $(this).attr('data-entity-position', count);
+            $(this).attr('target', '_blank');
+            $(this).attr('href', XowlClient.changeUrl($(this).attr('href')));
+            count++;
+        });
+        tinymce.activeEditor.dom.loadCSS('../wp-content/plugins/wp-xowl-client/assets/css/styles.css');
+        return $content.html();
+    };
+    XowlService.prototype.changeUrl = function(url) {
+        var patt = /(..\.)?(dbpedia.org\/resource\/)/;
+        var match = patt.exec(url);
+        if (match) {
+            var lang = (typeof match[1] === 'undefined') ? 'en.' : match[1];
+            url = url.replace(match[0], lang + 'wikipedia.org/wiki/');
+        }
+        return url;
+    }
+    var XowlClient = new XowlService();
     // Register the buttons
     tinymce.create('tinymce.plugins.xowl_plugin', {
-        init: function (ed) {
+        init: function(ed) {
             var self = this;
             self.firstLoad = true;
-            self.xsa = new XowlSemanticAdapter();
-
             // listen click inside textarea
-            ed.on('click', function (ev) {
-                tinymce.activeEditor.dom.loadCSS('../wp-content/plugins/wp-xowl-client/assets/css/styles.css');
-
+            ed.on('click', function(ev) {
                 if (!ev.target || ev.target.className === "") {
                     return;
                 }
-
-
-                 
                 // build select box
                 var entityPosition = ev.target.getAttribute('data-entity-position');
                 var bodyValues = [];
-
-
- 
-                if (typeof self.xsa.semantic[entityPosition] === 'undefined') {
+                if (typeof XowlClient.semantic[entityPosition] === 'undefined') {
                     return;
                 }
-
-                $.each(self.xsa.semantic[entityPosition].entities, function (i, e) {
-                    bodyValues.push({text: e["rdfs:label"].value + ' (' + e.uri + ')', value: e.uri});
+                $.each(XowlClient.semantic[entityPosition].entities, function(i, e) {
+                    var label = "";
+                    if (typeof e["rdfs:label"].value === "string") {
+                        label = e["rdfs:label"].value;
+                    } else {
+                        label = e["rdfs:label"][0].value;
+                    }
+                    bodyValues.push({
+                        text: label + ' (' + XowlClient.changeUrl(e.uri) + ')',
+                        value: XowlClient.changeUrl(e.uri)
+                    });
                 });
-
                 var listBox = new tinymce.ui.ListBox({
                     name: 'selectedEntity',
                     label: 'Select Entities',
                     values: bodyValues
                 });
                 listBox.entityPosition = entityPosition;
-
                 tinymce.activeEditor.windowManager.open({
                     elem: this,
                     title: 'Select Entity Dialog',
                     body: [listBox],
-                    buttons: [
-                        {
-                            text: 'Aceptar',
-                            onclick: function () {
-                                this.parent().fire('submit', {entityPosition: entityPosition});
-                            }
-                        },
-                        {
-                            text: 'Cancelar',
-                            onclick: function () {
-                                this.parent().fire('cancel');
-                            }
-                        },
-                        {
-                            text: 'Remove',
-                            onclick: function () {
-                                self.xsa.removeEntity(entityPosition);
-                                this.parent().fire('cancel');
-                            }
+                    buttons: [{
+                        text: 'Aceptar',
+                        onclick: function() {
+                            this.parent().fire('submit', {
+                                entityPosition: entityPosition
+                            });
                         }
-                    ],
+                    }, {
+                        text: 'Cancelar',
+                        onclick: function() {
+                            this.parent().fire('cancel');
+                        }
+                    }, {
+                        text: 'Remove',
+                        onclick: function() {
+                            self.xsa.removeEntity(entityPosition);
+                            this.parent().fire('cancel');
+                        }
+                    }],
                     // rebuild content inside textarea
-                    onsubmit: function (ev) {
-//                        console.log('ev.selectedEntity', ev.entityPosition);
-//                        self.xsa.removeEntity(entityPosition);
-//                        tinymce.activeEditor.setContent(self.xsa.textToEditor());
-                    }
+                    onsubmit: function(ev) {}
                 });
-
             });
             // init xsa object
-            ed.on('LoadContent', function () {
-                self.firstLoad = true;
-                self.xsa.buildFromData({semantic: [], text: tinymce.activeEditor.getContent()});
-            });
+            ed.on('LoadContent', function() {});
             // Enhance your content
             ed.addButton('xowl_button', {
                 title: 'Enhance your content',
-                //TODO: Search for a better Xowl icon
                 image: '../wp-content/plugins/wp-xowl-client/assets/imgs/enhance.png',
-                onclick: function () {
+                onclick: function() {
                     // add loader effect
                     $loader = $("<div/>", {
                         "class": 'loader'
@@ -93,54 +132,42 @@
                     $('body').css("position", "relative").append($loader);
                     // make request and replace content according with response
                     // clean content before and after response
-                    var content = tinymce.activeEditor.getContent() ;
-
+                    var content = tinymce.activeEditor.getContent();
                     $.post(xowlPlugin.xowl_endpoint, {
                         token: xowlPlugin.xowl_apikey,
-                        content: self.xsa.preClean( tinymce.activeEditor.getContent() )
-                    }).done(function (data) {
+                        content: XowlClient.preClean()
+                    }).done(function(data) {
                         $loader.remove();
-                        self.xsa = new XowlSemanticAdapter();
-                        tinymce.activeEditor.setContent( self.xsa.postReceive( self.xsa.buildFromData(data) )  );
-                    }).fail(function () {
+                        XowlClient.setContent(XowlClient.postReceive(XowlClient.buildFromData(data)));
+                    }).fail(function() {
                         $loader.remove();
-                        self.xsa = new XowlSemanticAdapter();
                         alert("Error retrieving content from Xowl Service.\nDo you have a valid API-KEY?");
                     });
                 }
             });
         }
     });
-
     /* Start the buttons */
     tinymce.PluginManager.add('xowl_button', tinymce.plugins.xowl_plugin);
-
-// ----------------------------------------------------------------------------
-
+    // ----------------------------------------------------------------------------
     // helper class to manage text and other data
     // maintains text and semantic nodes and serve data to editor
     function XowlSemanticAdapter() {
         this.semanticIndex = [];
         this.elementsData = [];
         this.semanticText = '';
-        this.saveAttr = {'class': 1, 'href': 1, 'target': 1};
+        this.saveAttr = {
+            'class': 1,
+            'href': 1,
+            'target': 1
+        };
     }
-
-    // parse server response and store in inner variables
-    XowlSemanticAdapter.prototype.buildFromData = function (data) {
-        var self = this;
-        self.semantic = data.semantic;
-        return data.text ; 
-    };
-
-  
     // return html with restricted attributes
-    XowlSemanticAdapter.prototype.modifyEntity = function (index, newHref) {
+    XowlSemanticAdapter.prototype.modifyEntity = function(index, newHref) {
         var self = this;
         if (!self.ok) {
             return;
         }
-
         var idx = self.semanticIndex[index];
         var elem = self.elementsData[idx].elem;
         var jq = $(elem);
@@ -149,80 +176,11 @@
         var wrap = $("<div>").append(jq);
         self.elementsData[idx].elem = wrap.html();
     };
-
     // return html with restricted attributes
-    XowlSemanticAdapter.prototype.removeEntity = function (position) {
-
-        var oldContent = $('<div></div>').append(  $.parseHTML( tinymce.activeEditor.getContent() ) );
-
+    XowlSemanticAdapter.prototype.removeEntity = function(position) {
+        var oldContent = $('<div></div>').append($.parseHTML(tinymce.activeEditor.getContent()));
         var element = $('a.xowl-suggestion[data-entity-position="' + position + '"]', oldContent).eq(0);
         element.replaceWith(element.html());
         tinymce.activeEditor.setContent(oldContent.html());
     };
-
-
-     XowlSemanticAdapter.prototype.preClean = function (content ) {
-        // returns string 
-         var $content = $('<div></div>').append(  $.parseHTML( content) );
-
-        $('a', $content).each( function() {
-            if ( typeof $(this).attr( "data-cke-suggestions") == 'string' ) {
-
-                $(this).removeAttr("data-cke-suggestions" );
-                $(this).removeAttr("data-entity-position" );
-                $(this).removeAttr("data-cke-annotation" );
-                $(this).removeAttr("data-cke-type" );
-                $(this).removeClass("xowl-suggestion" );
-            }  else {
-                return ;
-            }
-
-        });
-     
-        return $content.html(); 
-
-     } ;
-
-
-     XowlSemanticAdapter.prototype.postReceive = function (content ) {
-        // returns string 
-         var $content = $('<div></div>').append(  $.parseHTML( content) );
-
-        var count = 0 ; 
-
-                var changeHref = function (oldHref) {
-            var patt = /(..\.)?(dbpedia.org\/resource\/)/;
-            var match = patt.exec(oldHref);
-            if (match) {
-                var lang = (typeof match[1] === 'undefined') ? 'en.' : match[1];
-                return oldHref.replace(match[0], lang + 'wikipedia.org/wiki/');
-            }
-            return oldHref;
-        };
-
-
-        $( 'a.xowl-suggestion', $content ).each( function() {
-                $(this).attr('data-entity-position', count );
-                $(this).attr('target', '_blank');
-              //   $(this).attr( 'href', changeHref( $(this).attr( 'href')));
-                count++ ;
-
-
-        });
-
-
-
-
-
- 
-
-
-      
-        return $content.html(); 
-
-     } ;
-
-
-
-
 })(jQuery);
